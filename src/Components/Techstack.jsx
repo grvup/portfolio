166a2c6886fa2547ad64745eaca6d0da79e0d3dc
grvup <upadhyaygaurav450@gmail.com>
2024-08-06 22:58:10 +0530

    first

---
+++ src/Components/Techstack.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJava,
+  DiDjango,
+  DiGoogleCloudPlatform,
+} from "react-icons/di";
+import {
+  SiRedis,
+  SiFirebase,
+  SiMysql,
+} from "react-icons/si";
+
+
+function Techstack() {
+  return (
+    <div className="tech-div">
+      <Col xs={4} md={2} className="tech-icons" style={{backgroundColor:"black"}} >
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDjango />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedis />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGoogleCloudPlatform/>
+      </Col>
+
+    </div>
+  );
+}
+
+export default Techstack;
---
+++ src/Components/Toolstack.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiVercel,
+  SiEclipseide,
+  SiJupyter,
+  
+  SiXampp,
+  SiLinux,
+} from "react-icons/si";
+import { DiCompass } from "react-icons/di";
+function Toolstack() {
+  return (
+    <div className="tech-div" >
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title="Postman">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiEclipseide />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJupyter />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCompass />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiXampp />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
+      </Col>
+
+    </div>
+  );
+}
+
+export default Toolstack;
